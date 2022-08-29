06e1b84285955ed60b21271dbadfc20dfa284461
jdel54 <101069613+jdel54@users.noreply.github.com>
2022-08-29 20:02:59 +0200

    bold + br

--- pages/index.js
+++ pages/index.js
@@ -102,23 +102,23 @@ const Home = () => (
         </Heading>
         <BioSection>
         <BioYear>Feb2021 - July 2022</BioYear>
-        CTO, COO and Cofounder(Medifresco)
-        Startup e-commerce of Meal Kits based in Mallorca. Meal kit service supported with a physical store. +70k€ revenue in 1 Year. In charge of building the platform and digital structure detecting dropoffs and all posible CRO improvement strategies (Digital and Operational)
+        <br/><b>[CTO, COO and Cofounder(Medifresco)]</b>
+        <br/>Startup e-commerce of Meal Kits based in Mallorca. Meal kit service supported with a physical store. +70k€ revenue in 1 Year. In charge of building the platform and digital structure detecting dropoffs and all posible CRO improvement strategies (Digital and Operational)
         </BioSection>
         <BioSection>
         <BioYear>Sept 2019 - March 2021</BioYear>
-        Project Management (Umlaut Strategy Consulting for AIRBUS)
-        Leading and optimizing change processes of airplanes (4M€ Budget) applying project management methodologies. Alignment between different parties (customer and supplier) and product iterations. Detecting automation and digitalization opportunities.Supported and documented a new manufacturing line and aligned requirements between AIRBUS and Supplier
+        <br/><b>[Project Management (Umlaut Strategy Consulting for AIRBUS)]</b>
+        <br/>Leading and optimizing change processes of airplanes (4M€ Budget) applying project management methodologies. Alignment between different parties (customer and supplier) and product iterations. Detecting automation and digitalization opportunities.Supported and documented a new manufacturing line and aligned requirements between AIRBUS and Supplier
         </BioSection>
         <BioSection>
         <BioYear>Sept 2017 - Sept 2019</BioYear>
-        Project Management (VIE program for AIRBUS)
-        Supported directly the Head of the department and subteams and led the digitalization of the department, ensuring traversal participation between all involved parties. Automated and digitalized opportunities applying project management skills (+500k€ managed), iterating MVP until final version. In parallel, I managed risks and possible opportunities (low risk-high opportunity)
+        <br/><b>Project Management (VIE program for AIRBUS)</b>
+        <br/>Supported directly the Head of the department and subteams and led the digitalization of the department, ensuring traversal participation between all involved parties. Automated and digitalized opportunities applying project management skills (+500k€ managed), iterating MVP until final version. In parallel, I managed risks and possible opportunities (low risk-high opportunity)
         </BioSection>
         <BioSection>
           <BioYear>Jul 2016 - Jul 2017</BioYear>
-          AIRBUS trainee (Global supply chain Factory)
-          Official point of contact between supplier and production
+          <br/><b>AIRBUS trainee (Global supply chain Factory)</b>
+          <br/>Official point of contact between supplier and production
           Advanced control of raw material needs
           Exhaustive control of supply needs within the production site
           Stock controlling
@@ -139,27 +139,28 @@ const Home = () => (
         </Heading>
         <BioSection>
         <BioYear>August 2018 - February 2021</BioYear>
-        MIT University (online)
-        MicroMasters in supply chain Management with focus on the whole supply chain process
+        <br/><b>MIT University (online)</b>
+        <br/>MicroMasters in supply chain Management with focus on the whole supply chain process
         </BioSection>
         <BioSection>
         <BioYear>August 2016 - August 2017</BioYear>
-        Universidad de Alcalá de Henares (Spain)
-        Master with focus in: Development in companies, strategy, efficiency,communication, economics international business law, and finance
+        <br/><b>Universidad de Alcalá de Henares (Spain)</b>
+        <br/>Master with focus in: Development in companies, strategy, efficiency,communication, economics international business law, and finance
         </BioSection>
         <BioSection>
         <BioYear>Feb 2015 - August 2015</BioYear>
-        University of Münster (Germany)
-        Granted scholarship in an exchange program to complete my final bachelor thesis
+        <br/><b>University of Münster (Germany)</b>
+        <br/>Granted scholarship in an exchange program to complete my final bachelor thesis
         </BioSection>
         <BioSection>
         <BioYear>Sept 2010 - Sept 2015</BioYear>
-        Universidad de Córdoba (Spain)
-        Degree in Mechanical Engineering offering mixed and comprehensive training in the engineering field
+        <br/><b>Universidad de Córdoba (Spain)</b>
+        <br/>Degree in Mechanical Engineering offering mixed and comprehensive training in the engineering field
         </BioSection>
         <BioSection>
           <BioYear>Sept 2007 - Sept 2009</BioYear>
-          British School of Cordoba : British diploma (A-level, IGCSE) and Spanish diploma - Double high school diploma.
+          <br/><b>British School of Cordoba</b> 
+          <br/>British diploma (A-level, IGCSE) and Spanish diploma - Double high school diploma.
         </BioSection>
       </Section>
 
