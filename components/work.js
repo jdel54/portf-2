3f36dbda69ed3aff6966ea01af7adb21822251f2
jdel54 <101069613+jdel54@users.noreply.github.com>
2022-09-02 12:52:42 +0200

    removed links

--- components/work.js
+++ components/work.js
@@ -1,11 +1,11 @@
 import NextLink from 'next/link'
-import { Heading, Box, Image, Link, Badge } from '@chakra-ui/react'
+import { Heading, Box, Image, Badge } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 
 export const Title = ({ children }) => (
   <Box>
     <NextLink href="/works" passHref>
-      <Link>Works</Link>
+      Works
     </NextLink>
     <span>
       {' '}
