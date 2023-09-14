ff6922bf455d54f9e0378878d5d278f67eee2e02
jdel54 <101069613+jdel54@users.noreply.github.com>
2023-09-14 20:49:36 +0200

    firebase-revenue

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ const Home = () => (
           Improvement and development of new tools linked to SAP
         </BioSection>
         <Box align="center" my={4}>
-          <Link target="_blank" href="https://firebasestorage.googleapis.com/v0/b/jdel54-portfolio.appspot.com/o/Jorge_Del_Castillo_English.pdf?alt=media&token=255276e7-e3fe-4b8b-801d-fcebe65556f7" passHref scroll={false}>
+          <Link target="_blank" href="https://firebasestorage.googleapis.com/v0/b/jdel54-portfolio.appspot.com/o/Jorge_Del_Castillo_English.pdf?alt=media&token=262e2c97-b0f0-4240-921e-c56a2ebfba41" passHref scroll={false}>
             <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
               My Resumé
             </Button>
